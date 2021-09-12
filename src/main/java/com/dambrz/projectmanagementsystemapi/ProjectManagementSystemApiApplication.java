0e8581f1fb8cef8e9ae2ed0aa98fedc68df4fb26
dam-brz <dam-brz@wp.pl>
2021-09-12 14:48:16 +0200

    Initial commit

---
+++ src/main/java/com/dambrz/projectmanagementsystemapi/ProjectManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.dambrz.projectmanagementsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagementSystemApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dambrz/projectmanagementsystemapi/ProjectManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dambrz.projectmanagementsystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
