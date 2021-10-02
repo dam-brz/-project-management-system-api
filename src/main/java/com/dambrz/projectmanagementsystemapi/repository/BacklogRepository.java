f0e4dec3ca767acf096ac07e6255510353351556
dam-brz <dam-brz@wp.pl>
2021-10-02 13:45:27 +0200

    Create backlog and projectTask repository

---
+++ src/main/java/com/dambrz/projectmanagementsystemapi/repository/BacklogRepository.java
@@ -0,0 +1,9 @@
+package com.dambrz.projectmanagementsystemapi.repository;
+
+import com.dambrz.projectmanagementsystemapi.model.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+}
