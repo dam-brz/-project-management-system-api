fcfff06f798eae2ea1474bbf131c78efa8d5bc05
dam-brz <dam-brz@wp.pl>
2021-10-02 19:39:58 +0200

    Create method to persist project task

---
+++ src/main/java/com/dambrz/projectmanagementsystemapi/service/ProjectTaskService.java
@@ -0,0 +1,43 @@
+package com.dambrz.projectmanagementsystemapi.service;
+
+import com.dambrz.projectmanagementsystemapi.model.Backlog;
+import com.dambrz.projectmanagementsystemapi.model.ProjectTask;
+import com.dambrz.projectmanagementsystemapi.repository.BacklogRepository;
+import com.dambrz.projectmanagementsystemapi.repository.ProjectTaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectTaskService {
+
+    private final BacklogRepository backlogRepository;
+    private final ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTaskService(BacklogRepository backlogRepository, ProjectTaskRepository projectTaskRepository) {
+        this.backlogRepository = backlogRepository;
+        this.projectTaskRepository = projectTaskRepository;
+    }
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+
+        Backlog backlog = backlogRepository.findBacklogByProjectIdentifier(projectIdentifier);
+        projectTask.setBacklog(backlog);
+
+        int backlogSequence = backlog.getPTSequence();
+        backlogSequence++;
+        backlog.setPTSequence(backlogSequence);
+
+        projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+        if (projectTask.getPriority() == null || projectTask.getPriority() == 0) {
+            projectTask.setPriority(3);
+        }
+
+        if (projectTask.getStatus() == null || projectTask.getStatus().isBlank()) {
+            projectTask.setStatus("TO_DO");
+        }
+        return projectTaskRepository.save(projectTask);
+    }
+}
