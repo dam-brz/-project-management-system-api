1f2faad0dbf12b7a45dc94fc474db7d92507e961
dam-brz <dam-brz@wp.pl>
2021-11-14 11:09:59 +0100

    Refactor repository tests

--- src/test/java/com/dambrz/projectmanagementsystemapi/repository/BacklogRepositoryTest.java
+++ src/test/java/com/dambrz/projectmanagementsystemapi/repository/BacklogRepositoryTest.java
@@ -1,23 +1,16 @@
 package com.dambrz.projectmanagementsystemapi.repository;
 
 import com.dambrz.projectmanagementsystemapi.TestHelper;
-import com.dambrz.projectmanagementsystemapi.model.Backlog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
-//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class BacklogRepositoryTest extends TestHelper {
 
     @Test
     void shouldSaveBacklog() {
-        Backlog backlog = createValidSampleBacklog();
-        Backlog savedBacklog = backlogRepository.save(backlog);
-        assertThat(savedBacklog)
-                .usingRecursiveComparison()
-                .ignoringFields("id")
-                .isEqualTo(backlog);
+        assertThat(backlogRepository.save(createValidSampleBacklog())).isNotNull();
     }
 }
